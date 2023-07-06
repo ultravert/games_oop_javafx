4682b0577fe84d3679031841bfb0bbc115bb1237
ultravert <feodor.sax@gmail.com>
2023-07-06 19:51:06 +0500

    3. Каркас шахматной доски [#792 # [#792 #427250]]

--- chess/src/main/java/ru/job4j/chess/OccupiedCellException.java
+++ chess/src/main/java/ru/job4j/chess/OccupiedCellException.java
@@ -1,7 +1,7 @@
 package ru.job4j.chess;
 
 public class OccupiedCellException extends Exception {
-    public OccupiedCellException (String message) {
+    public OccupiedCellException(String message) {
         super(message);
     }
 }
